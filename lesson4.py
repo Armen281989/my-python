9a112ca2b3785f73051184877aeba7f03b9fe98f
Armen281989 <armenmardoyan89@gmail.com>
2020-02-29 20:13:48 +0400

    'homework4'

---
+++ lesson4.py
@@ -0,0 +1,60 @@
+# x = int(input('num ='))
+# print(x > 5)
+# print(x < 5)
+# print( x == 5)
+# print( x <= 5)
+# print( x >= 5)
+# print( x != 5)
+
+# import random
+# us = int(input('greq tiv '))
+# us1 = int(input('cer erkrod tiv '))
+# x = random.randint(1,100)
+# y = random.randint(1,100)
+# resiltmi = us + us1
+# resultcomp = x + y
+# print(resiltmi, resiltmi > resultcomp, resultcomp)
+
+# name = input('what is your name? ')
+# number = int(input('how old are you?'))
+# print(number > 18 and number < 27)
+# print(number > 18 or number < 27) 
+# print(not(number > 18 or number < 27))
+
+#  # imanum enq popoxakani hascen
+# name = 'armen'
+# print(id(name))
+# x = 65
+# print(id(x))
+ 
+
+#  #  stugum e hamapatasxanum en te voch
+# name = 'ani'
+# name1 = 'anna'
+# print(name is name1)
+
+# name = input('what is your name? ')
+# name2 = 'anna'
+# print(id(name))
+# print(id(name2))
+# print(name == 'anna' or name == 'Anna')
+
+# x = input('vorne amenaerkar get@?')
+# print(x == 'nexos'or x == 'Nexos' or x == 'NEXOS')
+# print(x == 'nexos'and x == 'Nexos' and x =='NEXOS')
+
+
+# name = 'ani'
+# name2 = 'anna'
+# print('a' in name2)
+
+# name1 = 'ani'
+# name3 = 'anna'
+# print('j' not in name2)
+
+# name = input('what is your name? ')
+# print('f'in name)
+
+# dasaran = 'artak gevor ruzan ashot valod ani anna gayan'
+# print('ani' in dasaran)
+# print('ani' not in dasaran and 'gevor' in dasaran or 'vaxarsh' in dasaran)

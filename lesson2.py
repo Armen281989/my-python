b59bfd736ac7dff731ad68ff16f2f4390e013352
Armen281989 <armenmardoyan89@gmail.com>
2020-02-29 20:01:24 +0400

    hi

---
+++ lesson2.py
@@ -0,0 +1,99 @@
+# dzer tariq
+# age = input("erb eq cnvel?")
+# x = 2020-int(age)
+# print("duq",x,"TAREKAN EQ")
+ 
+ # shrgani makeres
+# circle = int(input("aseq shrjani sharavix"))
+# raund = 3.14 * circle * circle
+# print("raund of circle is",raund)
+ 
+
+# type(tesak)- imanum popoxakani tesak(int,str,float)
+# x = 5
+# print(type(x))
+
+# y = 4.5
+# print(type(y))
+
+# z = "age"
+# print(type(z))
+
+ # plus
+# time = 5
+# minut = 9
+# plus = time+minut
+# print(plus)
+
+#  # minus
+# x = 14
+# num2 = 25
+# minus = x-num2
+# print(minus)
+
+#  # bazmapatkac
+# res = x*num2
+# print(res)
+# # bajanac
+# itok = x/num2
+# print(itok)
+
+# num = int(input('please enter first num?'))
+# num1 = int(input('please enter second num?'))
+# res = num-num1
+# print(res)
+
+# tiv poxel toxi
+# x = 5
+# z = 6
+# print(str(x)+str(z))
+
+# $ cuyc e talis mnacort
+# x = 11
+# y = 9
+# print(x%y)
+
+
+# //cuyc e talis qani angam ka amboxj tiv
+# x = 100
+# z = 25
+# print(x//z)
+
+# x = 6
+# y = 5
+# res = x**y
+# print(res)
+
+# circle = int(input("aseq shrjani sharavix"))
+# raund = 3.14 * circle ** 2
+# print("raund of circle"+' '+str(raund))
+
+# name = input('whot is your name?')
+# old = int(input('how old are you?'))
+# born = 2020-old
+# print('my name is'+' '+name+' '+'i was born'+' '+str(born))
+
+# erankyun = int(input('erankyan koxm'))
+# koxm = int(input('erankyan erkrord koxm'))
+# rum = erankyun ** 2 + koxm ** 2
+# result = rum ** 0.5
+# print(result)
+
+# num = int(input('greq tiv'))
+# result1 = num ** 0.5
+# print(result1)
+
+# balance = 100
+# paycheck = 150
+# balance = balance + paycheck
+# print(balance)
+
+# balance1 = 100
+# balance1 = balance1 + 150
+# balance1 += 150
+# print(balance1)
+
+# balance2 = 100
+# balance2 = balance2 - 150
+# balance2 -= 150
+# print(balance2)

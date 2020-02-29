a9a2ccef3f75ea08d5f0ab3bffc33dd30933c900
Armen281989 <armenmardoyan89@gmail.com>
2020-02-29 20:15:34 +0400

    'homework5'

---
+++ lesson5.py
@@ -0,0 +1,32 @@
+# print('please input (y) for right othervise no')
+# item_banana = input('have you banana in fridge? ') == 'y'
+# print(item_banana)
+
+
+# item_banana = input('have you banana in fridge? ') == 'y'
+# item_kiwi = input('have you kiwi in fridge? ') == 'y'
+# item_orange = input('have you orange in fridge? ') == 'y'
+# hom_electrocity = input('have you electrocity in home? ') == 'y'
+# remove_banana = item_banana and not hom_electrocity
+# remove_kiwi = item_kiwi and not hom_electrocity
+# remove_orange = item_orange and not hom_electrocity
+# print('hanel banan sarnaranic '+ str(remove_banana))
+# print('hanel banan sarnaranic '+ str(remove_kiwi))
+# print('hanel banan sarnaranic '+ str(remove_orange))
+# print('xanutic berel banana' + str(not item_banana))
+# print('xanutic berel kiwi' + str(not item_kiwi))
+# print('xanutic berel orange' + str(not item_orange))
+
+print('please input (y) for right othervise no')
+iphone = input('unes iphone? ') == 'y'
+print('unes iphone '+ str(iphone))
+telefon = input('inch model e cer iphone? ') == '7'
+iphone7 = iphone and telefon
+print('iphon 7 uneq ' + str(iphone7))
+guyn = input('iphone inch guyn e? ') == 'karmir'
+iphonguyn = iphone and guyn
+print('iphone guyn karmir ' + str(iphonguyn))
+
+zaryadshnik = input('unes zaryadshnik? ') == 'y'
+iphoneres = iphone and zaryadshnik
+print('heraxos dir zaryadki '+ str(iphoneres))

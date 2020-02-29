3fa4fa48e211e8d39618c7606ee35dc4c84cd0ab
Armen281989 <armenmardoyan89@gmail.com>
2020-02-29 20:18:09 +0400

    'p'

---
+++ p.py
@@ -0,0 +1,23 @@
+# print("helo world")
+# x=5
+# y=10
+# print(x+y)
+# astname = ("mardoyan")
+# print(name,lastname)
+# a=12
+# b=3.2
+# q=a*b
+# print(q)
+# name = "Armen"
+# lastname = "mardoyan"
+# x=2020
+# y=1989
+# z=x-y
+# print("i am", name,lastname,"i",z, "years old")
+# name = input("what is your name?")
+# year = input("how old are you?")
+# print("hi",name,year)
+name = input("dzer anun@?")
+email = input("greq dzer email")
+phone = input("greq dzer her. hamar@")
+print("es",name,"em,","my email",email+","+"my number",phone)

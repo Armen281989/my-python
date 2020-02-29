261dd28a0e097cae179f0bb2fb58eec2338ba368
Armen281989 <armenmardoyan89@gmail.com>
2020-02-29 20:06:52 +0400

    second commit

---
+++ lesson3.py
@@ -0,0 +1,48 @@
+# # from math import *
+# # # print(math.pi)
+# # # print(math.sqrt(16))
+
+# # shrjan = int(input('shrjanagic '))
+ 
+# # s = 2 * pi * shrjan ** 2
+# # print(pi)
+# # print(sqrt(16))
+# # import random
+# # # print(random.randint(1,6))
+
+# # x = random.randint(1,6)
+# # y = random.randint(1,6)
+# # print(x)
+# # print(y)
+# # print(x+y)
+
+# import random as r
+# print(r.randrange(0,100,2))
+
+
+# import time
+# x = int(input('x = '))
+# y = int(input('y = '))
+# time.sleep(2)
+# print('x + y = '+ str(x + y))
+
+# import os
+# print(os.getcwd())
+
+# import calendar
+# y = int(input('input the year:'))
+# m = int(input('input the month:'))
+# print(calendar.month(y,m))
+
+# import datetime
+# x = datetime.datetime.now()
+# print(x)
+# print(datetime.date.today())
+
+# import datetime
+# age = int(input('qani tarekan eq '))
+# x = datetime.datetime.now()
+# time = x.year - age
+# print('duq '+ str(time)+' tarekan eq cnvel')
+
+stackoverflow.com
